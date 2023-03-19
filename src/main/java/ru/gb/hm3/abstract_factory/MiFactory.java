dc1303ff2e5e69fd8cce94df37fe801e23292d0a
DeN <den.denis.denis@mail.ru>
2023-03-19 23:31:58 +0300

    hm3

--- src/main/java/ru/gb/hm3/abstract_factory/MiFactory.java
+++ src/main/java/ru/gb/hm3/abstract_factory/MiFactory.java
@@ -4,7 +4,6 @@ public class MiFactory implements ProductFactory {
     public String creatPhone() {
         return "Phone Xiaomi Mi 11 Lite 4G";
     }
-
     public String creatTV() {
         return "TV Xiaomi Mi TV 4S 65 T2S 65";
     }
